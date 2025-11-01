9c7462f0001399184bb86ff92fc76a8755be08a4
JumpyVi <jumpyvi@outlook.com>
2025-11-01 17:09:14 -0400

    Fix linting

--- Formula/rofi-calc.rb
+++ Formula/rofi-calc.rb
@@ -5,37 +5,39 @@ class RofiCalc < Formula
     sha256 "92cf4a1b2a42067d162cc7e87556644dab5db335cb3a65bc30e97467d7347b75"
     license "MIT"
     head "https://github.com/svenstaro/rofi-calc.git", branch: "master"
-    
+  
     livecheck do
       url :stable
       strategy :github_latest
     end
-    
+  
     bottle do
       root_url "https://github.com/jumpyvi/homebrew-morebrew/releases/download/rofi-calc-2.5.0"
       sha256 x86_64_linux: "010bee67584ef4580fa8bd00ffe2d344b5d6f295aa4f79648617a3711f956236"
     end
-    
+  
+    # Build dependencies
     depends_on "bison" => :build
     depends_on "check" => :build
     depends_on "meson" => :build
     depends_on "ninja" => :build
     depends_on "pkg-config" => :build
     depends_on "gcc" => :build
+  
+    # Runtime dependencies
     depends_on "rofi"
     depends_on "libqalculate"
-    depends_on :linux
+  
   
     def install
       system "meson", "setup", "build", *std_meson_args
       system "meson", "compile", "-C", "build", "--verbose"
-      
+  
       (lib/"rofi").install "build/src/libcalc.so" => "calc.so"
     end
-    
+  
     test do
-      assert_predicate lib/"rofi/calc.so", :exist?
+      assert_path_exists lib/"rofi/calc.so"
     end
   end
-  
   
