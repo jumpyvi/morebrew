778f81b89f9c8ccc46812cd9b5e8568439c89d5c
JumpyVi <jumpyvi@outlook.com>
2026-01-20 18:37:55 -0500

    Fix lint

--- Formula/dmidecode.rb
+++ Formula/dmidecode.rb
@@ -1,14 +1,9 @@
 class Dmidecode < Formula
-  desc "Dmidecode reports information about your system's hardware as described in your system BIOS according to the SMBIOS/DMI standard"
+  desc "Reports information about your hardware as described in your system BIOS"
   homepage "https://www.nongnu.org/dmidecode/"
   url "https://download.savannah.gnu.org/releases/dmidecode/dmidecode-3.6.tar.xz"
   sha256 "e40c65f3ec3dafe31ad8349a4ef1a97122d38f65004ed66575e1a8d575dd8bae"
   license "GPL-2.0-or-later"
-
-  livecheck do
-    url "https://download.savannah.gnu.org/releases/dmidecode/"
-    regex(/href=.*?dmidecode[._-]v?(\d+(?:\.\d+)+)\.tar\.xz/i)
-  end
 
   bottle do
     root_url "https://github.com/jumpyvi/homebrew-morebrew/releases/download/dmidecode-3.6"
